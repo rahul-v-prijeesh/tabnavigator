fe25c0f94bb322b8a0b19a4d19a4857cdd7bf2d0
rahul-v-prijeesh <76157423+rahul-v-prijeesh@users.noreply.github.com>
2021-08-30 16:40:54 +0530

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+export default class Facb extends React.Component {
+    render() {
+        return (
+         
+           
+           <Text>facebook</Text>
+        )
+      }
+    
+    
+    
+}
---
+++ screens/insta.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+export default class Instagram extends React.Component {
+    render() {
+        return (
+         
+           
+           <Text>instagram</Text>
+        )
+      }
+    
+    
+    }  
